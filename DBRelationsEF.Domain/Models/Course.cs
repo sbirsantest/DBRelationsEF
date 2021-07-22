395fcdac9f4fe974df7fbc32d65201a3d495f36c
sbarsan <sorinbarsan@gmail.com>
2021-07-22 17:51:00 +0300

    add 2 model classes : Author and Courses; these will be used to implement a one to many relationship - an author can have multiple courses but a course can have only one author

---
+++ DBRelationsEF.Domain/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBRelationsEF.Domain.Models
+{
+	public class Course
+	{
+      public int Id { get; set; }
+
+      public string Name { get; set; }
+
+      public string Description { get; set; }
+
+      public int Level { get; set; }
+
+      public float FullPrice { get; set; }
+   }
+}
